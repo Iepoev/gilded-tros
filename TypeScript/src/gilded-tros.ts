1ac81d76c22cd0f73e17d0a558c29932c6f32b21
Jeroen De Clerck <jeroen@vectera.com>
2023-07-24 19:13:10 +0200

    reinstate original items property by storing item ref in CategorizedItem

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -8,11 +8,11 @@ enum ItemCategories {
     SMELLY = 'smelly',
 }
 
-class CategorizedItem extends Item {
+class CategorizedItem {
     readonly MAX_QUALITY = 50;
     private readonly categoryUpdateMap: Record<ItemCategories, () => void> = {
         [ItemCategories.GENERIC]: this.updateGeneric.bind(this),
-        [ItemCategories.GOOD_WINE]: (() => this.quality = this.quality + 1).bind(this),
+        [ItemCategories.GOOD_WINE]: (() => this.item.quality = this.item.quality + 1).bind(this),
         [ItemCategories.LEGENDARY]: () => null,
         [ItemCategories.BACKSTAGE_PASS]: this.updateBackstagePass.bind(this),
         [ItemCategories.SMELLY]: this.updateSmelly.bind(this),
@@ -27,15 +27,16 @@ class CategorizedItem extends Item {
     }
 
     private category: ItemCategories
+    private item: Item
 
     constructor(item: Item) {
-        super(item.name, item.sellIn, item.quality);
+        this.item = item;
         this.category = this.nameToCategory(item.name);
     }
 
     private nameToCategory(name: string) : ItemCategories {
         for (const [category, regex] of Object.entries(this.categoryKeywordsMap)) {
-            if(regex?.test(this.name.toLowerCase())){
+            if(regex?.test(this.item.name.toLowerCase())){
                 return category as ItemCategories
             }
         }
@@ -45,43 +46,51 @@ class CategorizedItem extends Item {
     public updateQuality(): void {
         this.categoryUpdateMap[this.category]();
         if(this.category !== ItemCategories.LEGENDARY) {
-            this.quality = Math.min(Math.max(this.quality, 0), this.MAX_QUALITY)
-            this.sellIn = this.sellIn -1
+            this.item.quality = Math.min(Math.max(this.item.quality, 0), this.MAX_QUALITY)
+            this.item.sellIn = this.item.sellIn -1
         }
     }
 
+    public isEqual(item: Item) {
+        return this.item.name === item.name;
+    }
+
     private updateGeneric(): void {
-        this.quality = this.sellIn > 0 ? this.quality - 1 : this.quality - 2
+        this.item.quality = this.item.sellIn > 0 ? this.item.quality - 1 : this.item.quality - 2
     }
 
     private updateBackstagePass(): void {
-        if(this.sellIn <= 0) {
-            this.quality = 0;
-        } else if (this.sellIn <= 5) {
-            this.quality = this.quality + 3;
-        } else if (this.sellIn <= 10) {
-            this.quality = this.quality + 2;
+        if(this.item.sellIn <= 0) {
+            this.item.quality = 0;
+        } else if (this.item.sellIn <= 5) {
+            this.item.quality = this.item.quality + 3;
+        } else if (this.item.sellIn <= 10) {
+            this.item.quality = this.item.quality + 2;
         } else {
-            this.quality = this.quality + 1;
+            this.item.quality = this.item.quality + 1;
         }
     }
 
     private updateSmelly(): void {
-        this.quality = this.sellIn > 0 ? this.quality - 2 : this.quality - 4
+        this.item.quality = this.item.sellIn > 0 ? this.item.quality - 2 : this.item.quality - 4
     }
 }
 
 
 export class GildedTros {
-    public items : CategorizedItem[];
+    public categorizedItems : CategorizedItem[];
 
-    constructor(inputItems: Item[]) {
-        this.items = inputItems.map(item => new CategorizedItem(item));
+    constructor(public items: Array<Item>) {
+        this.categorizedItems = items.map(item => new CategorizedItem(item));
     }
 
     public updateQuality(): void {
-        this.items.forEach(item => {
-            item.updateQuality();
+        this.items.forEach((item, index) => {
+            const categorizedItem = this.categorizedItems[index];
+            if(!categorizedItem.isEqual(item)) {
+                throw Error(`categorized item list out of sync! could not find ${item.name} at index ${index}`,)
+            }
+            categorizedItem.updateQuality();
         });
     }
 }
